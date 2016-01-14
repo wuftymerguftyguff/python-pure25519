180dcb1b5be063b9074942ad174103f5c2371064
Jeff Arthur <wuftymerguftyguff@gmail.com>
2016-01-14 09:52:37 +0000

    add base64_urlsafe
    add base64_urlsafe encoding support for sigs and keys to oop
    implementation.

--- pure25519/ed25519_oop.py
+++ pure25519/ed25519_oop.py
@@ -23,6 +23,7 @@ def remove_prefix(s_bytes, prefix):
 def to_ascii(s_bytes, prefix="", encoding="base64"):
     """Return a version-prefixed ASCII representation of the given binary
     string. 'encoding' indicates how to do the encoding, and can be one of:
+     * base64_urlsafe
      * base64
      * base32
      * base16 (or hex)
@@ -40,7 +41,9 @@ def to_ascii(s_bytes, prefix="", encoding="base64"):
     assert isinstance(s_bytes, bytes)
     if not isinstance(prefix, bytes):
         prefix = prefix.encode('ascii')
-    if encoding == "base64":
+    if encoding == "base64_urlsafe":
+        s_ascii = base64.urlsafe_b64encode(s_bytes).decode('ascii').rstrip("=")
+    elif encoding == "base64":
         s_ascii = base64.b64encode(s_bytes).decode('ascii').rstrip("=")
     elif encoding == "base32":
         s_ascii = base64.b32encode(s_bytes).decode('ascii').rstrip("=").lower()
@@ -59,7 +62,10 @@ def from_ascii(s_ascii, prefix="", encoding="base64"):
     if isinstance(prefix, bytes):
         prefix = prefix.decode('ascii')
     s_ascii = remove_prefix(s_ascii.strip(), prefix)
-    if encoding == "base64":
+    if encoding == "base64_urlsafe":
+        s_ascii += "="*((4 - len(s_ascii)%4)%4)
+        s_bytes = base64.urlsafe_b64decode(str(s_ascii))
+    elif encoding == "base64":
         s_ascii += "="*((4 - len(s_ascii)%4)%4)
         s_bytes = base64.b64decode(s_ascii)
     elif encoding == "base32":
